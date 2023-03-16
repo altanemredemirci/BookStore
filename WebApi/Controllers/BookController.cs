a27e7cdebda17068ee94b95a92b0d8460d91cc77
altanemredemirci <altan_emre_1989@hotmail.com>
2023-03-16 16:46:40 +0300

    RestfullApi Get

--- WebApi/Controllers/BookController.cs
+++ WebApi/Controllers/BookController.cs
@@ -1,12 +1,43 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace WebApi.Controllers;
+namespace WebApi.Controllers
 {
     [ApiController]
     [Route("[controller]s")]
-    public class BookController:ControllerBase
+    public class BookController : ControllerBase
     {
-        public static List<Book>  
+        public static List<Book> BookList = new List<Book>(){
+            new Book{
+                Id=1,
+                Title="Simyacı",
+                GenreId=1,
+                PageCount=200,
+                PublishDate=new DateTime(1999,01,01)
+            },
+            new Book{
+                Id=2,
+                Title="Martı",
+                GenreId=2,
+                PageCount=200,
+                PublishDate=new DateTime(1997,01,01)
+            },
+            new Book{
+                Id=3,
+                Title="Metal Fırtına",
+                GenreId=3,
+                PageCount=300,
+                PublishDate=new DateTime(2005,01,01)
+            }
+        };
+
+
+        [HttpGet]
+        public List<Book> Get()
+        {
+            var bookList = BookList.OrderBy(i => i.Id).ToList<Book>();
+            return bookList;
+        }
+       
 
     }
 }
