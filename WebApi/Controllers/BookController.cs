a19e46896d619c80cffb142854dba226c8668db5
altanemredemirci <altan_emre_1989@hotmail.com>
2023-03-16 15:55:04 +0300

    First Commit

---
+++ WebApi/Controllers/BookController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+{
+    [ApiController]
+    [Route("[controller]s")]
+    public class BookController:ControllerBase
+    {
+        public static List<Book>  
+
+    }
+}
